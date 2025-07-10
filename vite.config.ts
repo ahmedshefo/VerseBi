898ea78d9ad16968a5157ecfa6e8984661ad3989
ahmedshefo <76793227+ahmedshefo@users.noreply.github.com>
2025-07-10 06:58:17 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // <-- THIS IS IMPORTANT
+  base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
